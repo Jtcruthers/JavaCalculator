e470c1a2bddaf6d5f7311ae56543c5329cffa59f
Ryan Tomlinson <ryantomlinson95@gmail.com>
2015-03-11 17:36:41 -0400

    Added View Model Controller classes

---
+++ src/com/bigfoot/javacalc/DoubleCalculator.java
@@ -0,0 +1,40 @@
+package com.bigfoot.javacalc;
+
+/**
+ * Double Calculator application.
+ *
+ * @author Ryan Tomlinson and Justin Carruthers
+ *
+ */
+public final class DoubleCalculator {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private DoubleCalculator() {
+    }
+
+    /**
+     * Main program that sets up main application window and starts user
+     * interaction.
+     *
+     * @param args
+     *            command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        /*
+         * Create instances of the model, view, and controller objects;
+         * controller needs to know about model and view, and view needs to know
+         * about controller
+         */
+
+//        DoubleCalcModel model = new DoubleCalcModel1();
+//        DoubleCalcView view = new DoubleCalcView1();
+//        DoubleCalcController controller = new DoubleCalcController1(model, view);
+
+        /*
+         * view.registerObserver(controller);
+         */
+
+    }
+}
